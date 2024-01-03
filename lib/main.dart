d91a7fecffbaed3344eef2a164991ae25b785474
RaxoTheOne <traxon.bg@gmail.com>
2024-01-03 13:24:39 +0100

    saved the change

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_framework.dart';
-import 'package:responsive_framework/responsive_wrapper.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      builder: (context, widget) => ResponsiveWrapper.builder(
-        BouncingScrollWrapper.builder(context, widget!),
-        maxWidth: 1200,
-        minWidth: 450,
-        defaultScale: true,
-        breakpoints: [
-          ResponsiveBreakpoint.resize(450, name: MOBILE),
-          ResponsiveBreakpoint.autoScale(800, name: TABLET),
-          ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-        ],
-        background: Container(color: Color(0xFFF5F5F5))),
-      home: Scaffold(
-        body: ResponsiveGridView.builder(
-          gridDelegate: ResponsiveGridDelegate(
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-          ),
-          itemBuilder: (context, index) => Container(
-            alignment: Alignment.center,
-            color: Colors.blue[(index+1)*100],
-          ),
-          itemCount: 10,
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Responsive GridView'),
+      ),
+      body: ResponsiveGridView.builder(
+        gridDelegate: const ResponsiveGridDelegate(
+          crossAxisExtent: 150, // Breite eines Elements
+          crossAxisSpacing: 8,   // Abstand zwischen den Elementen
+          mainAxisSpacing: 8,     // Vertikaler Abstand zwischen den Reihen
         ),
+        itemCount: 6,  // Anzahl der Container
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            color: _getColor(index), // Funktion zum Abrufen der Farbe
+            height: 100,
+            width: 150,
+            child: Center(
+              child: Text('Element $index'),
+            ),
+          );
+        },
       ),
     );
+  }
+
+  // Funktion zum Abrufen der Farbe basierend auf dem Index
+  Color _getColor(int index) {
+    return index % 2 == 0 ? Colors.blue : Colors.green;
   }
 }
